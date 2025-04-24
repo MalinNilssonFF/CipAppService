d587d27c9e8c07b6c19d63922294768cb63377b8
MalinNilssonFF <malin.nilsson@forefront.se>
2025-04-24 12:40:22 +0200

    .

--- CipAppService/CipWebApi/Controllers/WeatherForecastController.cs
+++ CipAppService/CipWebApi/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace CipWebApi.Controllers
 {
@@ -21,11 +22,16 @@ namespace CipWebApi.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            var encoded = Request.Headers["x-ms-client-principal"];
+            var decoded = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(encoded));
+           // var principal = JsonSerializer.Deserialize<ClientPrincipal>(decoded);
+
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)] + " " + decoded
             })
             .ToArray();
         }
